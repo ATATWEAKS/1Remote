c37db8a8849d2ef671496594a313100a8caaf019
Shawn <veckshawn@gmail.com>
2024-03-02 17:13:33 +0800

    [WIP] integrate RdpForm

--- Ui/View/Host/ProtocolHosts/IntegrateHost.xaml.cs
+++ Ui/View/Host/ProtocolHosts/IntegrateHost.xaml.cs
@@ -309,7 +309,7 @@ namespace _1RM.View.Host.ProtocolHosts
             Status = ProtocolHostStatus.Connecting;
             Debug.Assert(ParentWindow != null);
 
-            var tsk = new Task(Start);
+            var tsk = new Task(StartExe);
             tsk.Start();
         }
 
@@ -371,7 +371,7 @@ namespace _1RM.View.Host.ProtocolHosts
             });
         }
 
-        public void Start()
+        private void StartExe()
         {
             RunBeforeConnect?.Invoke();
             var exeFullName = ExeFullName;
